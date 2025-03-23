a2393801dc5e5915f344b44f746f790d0e5de80c
JohnDin016 <jalaldous6@gmail.com>
2025-03-23 11:05:41 +0100

    Fix internal server error

--- app.py
+++ app.py
@@ -76,6 +76,10 @@ def index():
 
     return render_template("index.html", result=result)
 
+@app.route("/", methods=["GET", "HEAD"])
+def home():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
